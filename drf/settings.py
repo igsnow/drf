a0ce854c3619a273a9624f1eaf08eab4d468a896
igsnow <zzy716810918@gmail.com>
2020-06-09 10:51:59 +0800

    添加api文档扩展

--- drf/settings.py
+++ drf/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'drf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'drfdb',
+        'USER': 'root',
+        'PASSWORD': '1017',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
